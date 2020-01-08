66c8aba7e7cfe085a1a5329b64920acaf2fe83ab
YUCEN CAO <caoyucen@gmail.com>
2020-01-08 15:24:00 -0800

    4

--- P4/p4/p4/client.c
+++
@@ -1,9 +0,0 @@
-//
-//  client.c
-//  p4
-//
-//  Created by Yucen Cao on 12/5/19.
-//  Copyright © 2019 Yucen Cao. All rights reserved.
-//
-
-#include <stdio.h>
--- P4/p4/p4/otp_dec_d.c
+++
@@ -1,9 +0,0 @@
-//
-//  otp_dec_d.c
-//  p4
-//
-//  Created by Yucen Cao on 12/6/19.
-//  Copyright © 2019 Yucen Cao. All rights reserved.
-//
-
-#include <stdio.h>
